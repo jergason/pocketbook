d1fdaeb7b0adbde7755d2cd85e7f3dcf231a108b
Jamison Dance <jergason@gmail.com>
2010-10-28 14:33:03 -0600

    flesh out controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def about
+    @title = "About"
   end
 
   def help
+    @title = "Help"
   end
 
 end
--- app/controllers/raw_data_sets_controller.rb
+++ app/controllers/raw_data_sets_controller.rb
@@ -1,5 +1,13 @@
 class RawDataSetsController < ApplicationController
   def show
+    @title = "Show"
+    @raw_data_set = RawDataSet.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.xml { render :json => @raw_data_set.to_json }
+      format.json { render :xml => @raw_data_set.to_xml }
+    end
   end
 
   def index
