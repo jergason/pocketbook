b0336af7b398fbc741d1233d64b6949f42e0d9f4
Jamison Dance <jergason@gmail.com>
2010-11-19 14:43:41 -0700

    add more tests

--- lib/modules/filesystem_helpers.rb
+++ lib/modules/filesystem_helpers.rb
@@ -1,11 +1,10 @@
 #General Filesystem Stuff
 module FilesystemHelpers
   def compress_folder(folder_path)
+    compressed_path = "#{folder_path}.tar"
     compress_command = "tar -czf #{folder_path}.tar #{folder_path}/*"
     puts compress_command
     system(compress_command)
-    folder_path + ".tar"
+    compressed_path
   end
 end
-
-puts "loaded filesystem helpers"
